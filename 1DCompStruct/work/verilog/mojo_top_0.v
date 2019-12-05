0a15bab73013b1b509857a49c6e8f5ae1dab3e49
mjosephan2 <mario.kosasih@gmail.com>
2019-12-05 22:25:50 +0800

    YES DONE

--- 1DCompStruct/work/verilog/mojo_top_0.v
+++ 1DCompStruct/work/verilog/mojo_top_0.v
@@ -31,7 +31,8 @@ module mojo_top_0 (
     input player2leftBtn,
     input player2rightBtn,
     input player1leftBtn,
-    input player1rightBtn
+    input player1rightBtn,
+    input startBtn
   );
   
   
@@ -81,11 +82,53 @@ module mojo_top_0 (
     .in(M_player2right_cond_in),
     .out(M_player2right_cond_out)
   );
+  wire [1-1:0] M_bigbtn_cond_out;
+  reg [1-1:0] M_bigbtn_cond_in;
+  button_conditioner_2 bigbtn_cond (
+    .clk(clk),
+    .in(M_bigbtn_cond_in),
+    .out(M_bigbtn_cond_out)
+  );
+  wire [1-1:0] M_edge_out;
+  reg [1-1:0] M_edge_in;
+  edge_detector_7 L_edge (
+    .clk(clk),
+    .in(M_edge_in),
+    .out(M_edge_out)
+  );
+  wire [1-1:0] M_edge1left_out;
+  reg [1-1:0] M_edge1left_in;
+  edge_detector_7 edge1left (
+    .clk(clk),
+    .in(M_edge1left_in),
+    .out(M_edge1left_out)
+  );
+  wire [1-1:0] M_edge1right_out;
+  reg [1-1:0] M_edge1right_in;
+  edge_detector_7 edge1right (
+    .clk(clk),
+    .in(M_edge1right_in),
+    .out(M_edge1right_out)
+  );
+  wire [1-1:0] M_edge2left_out;
+  reg [1-1:0] M_edge2left_in;
+  edge_detector_7 edge2left (
+    .clk(clk),
+    .in(M_edge2left_in),
+    .out(M_edge2left_out)
+  );
+  wire [1-1:0] M_edge2right_out;
+  reg [1-1:0] M_edge2right_in;
+  edge_detector_7 edge2right (
+    .clk(clk),
+    .in(M_edge2right_in),
+    .out(M_edge2right_out)
+  );
   wire [4-1:0] M_display1_pixel;
   wire [1-1:0] M_display1_led;
   reg [96-1:0] M_display1_color;
   reg [1-1:0] M_display1_update;
-  display_6 display1 (
+  display_12 display1 (
     .clk(clk),
     .rst(rst),
     .color(M_display1_color),
@@ -97,7 +140,7 @@ module mojo_top_0 (
   wire [1-1:0] M_display2_led;
   reg [96-1:0] M_display2_color;
   reg [1-1:0] M_display2_update;
-  display_6 display2 (
+  display_12 display2 (
     .clk(clk),
     .rst(rst),
     .color(M_display2_color),
@@ -109,7 +152,7 @@ module mojo_top_0 (
   wire [1-1:0] M_display3_led;
   reg [96-1:0] M_display3_color;
   reg [1-1:0] M_display3_update;
-  display_6 display3 (
+  display_12 display3 (
     .clk(clk),
     .rst(rst),
     .color(M_display3_color),
@@ -121,7 +164,7 @@ module mojo_top_0 (
   wire [1-1:0] M_display4_led;
   reg [96-1:0] M_display4_color;
   reg [1-1:0] M_display4_update;
-  display_6 display4 (
+  display_12 display4 (
     .clk(clk),
     .rst(rst),
     .color(M_display4_color),
@@ -133,7 +176,7 @@ module mojo_top_0 (
   wire [1-1:0] M_display5_led;
   reg [96-1:0] M_display5_color;
   reg [1-1:0] M_display5_update;
-  display_6 display5 (
+  display_12 display5 (
     .clk(clk),
     .rst(rst),
     .color(M_display5_color),
@@ -145,7 +188,7 @@ module mojo_top_0 (
   wire [1-1:0] M_display6_led;
   reg [96-1:0] M_display6_color;
   reg [1-1:0] M_display6_update;
-  display_6 display6 (
+  display_12 display6 (
     .clk(clk),
     .rst(rst),
     .color(M_display6_color),
@@ -167,17 +210,20 @@ module mojo_top_0 (
   wire [4-1:0] M_gamestates_car2Pos;
   wire [96-1:0] M_gamestates_rowcar;
   wire [8-1:0] M_gamestates_debug;
+  wire [16-1:0] M_gamestates_score_out;
   reg [1-1:0] M_gamestates_car1left;
   reg [1-1:0] M_gamestates_car1right;
   reg [1-1:0] M_gamestates_car2left;
   reg [1-1:0] M_gamestates_car2right;
-  gamestates_12 gamestates (
+  reg [1-1:0] M_gamestates_bigbtn;
+  gamestates_18 gamestates (
     .clk(clk),
     .rst(rst),
     .car1left(M_gamestates_car1left),
     .car1right(M_gamestates_car1right),
     .car2left(M_gamestates_car2left),
     .car2right(M_gamestates_car2right),
+    .bigbtn(M_gamestates_bigbtn),
     .row1(M_gamestates_row1),
     .row2(M_gamestates_row2),
     .row3(M_gamestates_row3),
@@ -191,7 +237,19 @@ module mojo_top_0 (
     .car1Pos(M_gamestates_car1Pos),
     .car2Pos(M_gamestates_car2Pos),
     .rowcar(M_gamestates_rowcar),
-    .debug(M_gamestates_debug)
+    .debug(M_gamestates_debug),
+    .score_out(M_gamestates_score_out)
+  );
+  
+  wire [16-1:0] M_alu_c;
+  reg [6-1:0] M_alu_alufn;
+  reg [16-1:0] M_alu_a;
+  reg [16-1:0] M_alu_b;
+  alu_19 alu (
+    .alufn(M_alu_alufn),
+    .a(M_alu_a),
+    .b(M_alu_b),
+    .c(M_alu_c)
   );
   
   always @* begin
@@ -205,17 +263,24 @@ module mojo_top_0 (
     io_seg = 8'hff;
     io_sel = 4'hf;
     M_player1left_cond_in = player1leftBtn;
-    player1left = M_player1left_cond_out;
+    M_edge1left_in = M_player1left_cond_out;
+    player1left = M_edge1left_out;
     M_player1right_cond_in = player1rightBtn;
-    player1right = M_player1right_cond_out;
+    M_edge1right_in = M_player1right_cond_out;
+    player1right = M_edge1right_out;
     M_player2left_cond_in = player2leftBtn;
-    player2left = M_player2left_cond_out;
+    M_edge2left_in = M_player2left_cond_out;
+    player2left = M_edge2left_out;
     M_player2right_cond_in = player2rightBtn;
-    player2right = M_player2right_cond_out;
+    M_edge2right_in = M_player2right_cond_out;
+    player2right = M_edge2right_out;
     M_gamestates_car1left = player1left;
     M_gamestates_car1right = player1right;
     M_gamestates_car2left = player2left;
     M_gamestates_car2right = player2right;
+    M_bigbtn_cond_in = startBtn;
+    M_edge_in = M_bigbtn_cond_out;
+    M_gamestates_bigbtn = M_edge_out;
     M_display1_update = 1'h1;
     M_display2_update = 1'h1;
     M_display3_update = 1'h1;
@@ -235,5 +300,11 @@ module mojo_top_0 (
     M_display6_color = M_gamestates_rowcar;
     row6 = M_display6_led;
     io_led[16+7-:8] = M_gamestates_debug;
+    M_alu_alufn = 6'h00;
+    M_alu_a = 16'h0000;
+    M_alu_b = 16'h0001;
+    io_led[16+7-:8] = M_gamestates_currentState;
+    io_led[8+7-:8] = M_gamestates_score_out;
+    io_led[0+7-:8] = {4'h8{M_gamestates_fail}};
   end
 endmodule
